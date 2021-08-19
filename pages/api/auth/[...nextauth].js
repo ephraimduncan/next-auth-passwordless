472b604ecfec2a73dce73813868a6b3de03d1f29
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-08-19 21:02:15 +0000

    change smtp server request format

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,14 +10,15 @@ const client = new Fauna.Client({
 export default NextAuth({
   providers: [
     Providers.Email({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
+      // server: {
+      //   host: process.env.EMAIL_SERVER_HOST,
+      //   port: process.env.EMAIL_SERVER_PORT,
+      //   auth: {
+      //     user: process.env.EMAIL_SERVER_USER,
+      //     pass: process.env.EMAIL_SERVER_PASSWORD,
+      //   },
+      // },
+      server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
     }),
   ],
