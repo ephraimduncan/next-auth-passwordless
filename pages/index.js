98158e08ba4b4d0f4e25ede6984c5aad6366a212
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-08-19 19:08:27 +0000

    add email authentication with client

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
+import { signIn, signOut, useSession } from "next-auth/client";
 import Laypout from "../components/Laypout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [session, loading] = useSession();
+
+  console.log(session);
+
   return (
     <Laypout>
       <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/index.js</code>
+        {!loading && !session && (
+          <>
+            <p>Get started by sigining in. </p>
+            <button className={styles.code} onClick={() => signIn()}>
+              Sign in
+            </button>
+          </>
+        )}
+        {session && !loading && (
+          <>
+            <p>
+              Signed in as{" "}
+              <span className={styles.p}>{session.user.email}</span>
+            </p>
+            <button className={styles.code} onClick={() => signOut()}>
+              Sign out
+            </button>
+          </>
+        )}
       </p>
     </Laypout>
   );
