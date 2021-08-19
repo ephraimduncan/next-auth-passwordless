3164817d7ab3b5c2f0b19889f285d826f368b4f4
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-08-19 20:27:21 +0000

    fix anonymous function in protected

--- pages/protected.js
+++ pages/protected.js
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/client";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
 
-export default function () {
+export default function Protected() {
   const [session, loading] = useSession();
 
   return (
